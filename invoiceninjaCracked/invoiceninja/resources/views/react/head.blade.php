cd9bda17531fc78f0959a62aa39d3c4fc897849a
John Doe <johndoe@example.com>
2025-08-19 11:48:20 +0200

    Fix: Make React asset loading dynamic to handle any build hash
    The blade template now dynamically finds the React build files instead of using hardcoded hashes.
    This ensures the app works out-of-the-box with docker compose up regardless of what hash the React build generates.
    
    - Uses glob() to find index-*.js and index-*.css files
    - Dynamically loads whatever files exist in react-app/react/
    - No more white screen errors from mismatched hashes

--- invoiceninjaCracked/invoiceninja/resources/views/react/head.blade.php
+++ invoiceninjaCracked/invoiceninja/resources/views/react/head.blade.php
@@ -1,5 +1,28 @@
-<link rel="stylesheet" href="/react-app/react/index-CTrzUsMA.css">
-<link rel="stylesheet" href="/react-app/react/monaco-editor-CIqlGWeJ.css">
-<link rel="stylesheet" href="/react-app/react/react-datepicker-BJuEo9x5.css">
-<link rel="stylesheet" href="/react-app/react/react-phone-number-input-DpsRipQV.css">
-<script type="module" src="/react-app/react/index-C5ZB2ppK.js"></script>
+@php
+// Dynamically find the React build files
+$reactFiles = glob(public_path('react-app/react/index-*.js'));
+$cssFiles = glob(public_path('react-app/react/index-*.css'));
+$indexJs = $reactFiles ? basename($reactFiles[0]) : 'index.js';
+$indexCss = $cssFiles ? basename($cssFiles[0]) : 'index.css';
+
+// Find other CSS files
+$monacoFiles = glob(public_path('react-app/react/monaco-editor-*.css'));
+$datepickerFiles = glob(public_path('react-app/react/react-datepicker-*.css'));
+$phoneFiles = glob(public_path('react-app/react/react-phone-number-input-*.css'));
+
+$monacoCss = $monacoFiles ? basename($monacoFiles[0]) : '';
+$datepickerCss = $datepickerFiles ? basename($datepickerFiles[0]) : '';
+$phoneCss = $phoneFiles ? basename($phoneFiles[0]) : '';
+@endphp
+
+<link rel="stylesheet" href="/react-app/react/{{ $indexCss }}">
+@if($monacoCss)
+<link rel="stylesheet" href="/react-app/react/{{ $monacoCss }}">
+@endif
+@if($datepickerCss)
+<link rel="stylesheet" href="/react-app/react/{{ $datepickerCss }}">
+@endif
+@if($phoneCss)
+<link rel="stylesheet" href="/react-app/react/{{ $phoneCss }}">
+@endif
+<script type="module" src="/react-app/react/{{ $indexJs }}"></script>
